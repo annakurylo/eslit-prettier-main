6748354fcb508c5308657a96329358209467877a
annakurylo <anna.k.2003.g@gmail.com>
2022-10-29 19:13:45 +0300

    ADD: husky & lint hook

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+    env: {
+        node: true,
+        es2021: true,
+    },
+    extends: ['standard', 'prettier', 'plugin:sonarjs/recommended'],
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: 'sonarjs',
+    rules: {},
+}
