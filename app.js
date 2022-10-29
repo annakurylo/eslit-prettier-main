4a9ecdb94466b654d86c387b88aa2bd2838ef990
annakurylo <anna.k.2003.g@gmail.com>
2022-10-29 20:05:17 +0300

    ADD: app

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 
 var indexRouter = require('./routes/index')
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/users')
 
 var app = express()
 
